2ef47658dbc900ef0e5ca515ee2f1b98a26f239e
MatthewDodi <matthew.dodi@gmail.com>
2018-11-08 02:05:39 -0800

    type: feat  - Finally! Implemented flip animation! Phew

--- Containers/Quiz.js
+++ Containers/Quiz.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text, TouchableOpacity, Dimensions, Animated } from 'react-native';
+import { View, Text, TouchableOpacity, Dimensions, Animated, StyleSheet } from 'react-native';
 import styled from 'styled-components/native';
 
-const QuizCard = styled.View`
-  padding: 5px;
-  background-color: ${props => props.color};
-  margin: 15px auto;
-  width: 300;
-  height: 460;
-  align-items: center;
-  justify-content: center;
-  border-radius: 5;
-  shadow-color: #000;
-  shadow-radius: 6;
-  shadow-opacity: .25;
-`
+const styles = StyleSheet.create({
+  QuizCard : {
+    padding: 5,
+    margin: 15,
+    width: 300,
+    height: 460,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    shadowColor: '#000',
+    shadowRadius: 6,
+    shadowOpacity: .25,
+    shadowOffset: {height: 10},
+    width: .95 * Dimensions.get('window').width,
+    backfaceVisibility: 'hidden'
+}});
 
 const AnswerButton = styled.TouchableOpacity`
   margin: 5px;
@@ -39,9 +42,30 @@ const Title = styled.Text`
 
 class Quiz extends Component {
   state = {
+    right: 0,
+    wrong: 0,
     questionCounter : 0,
-    flipAnimationValue : new Animated.Value(0)
+    rotate : new Animated.Value(0),
+    animationValue: 0
+  };
+
+  componentDidMount() {
+    this.state.rotate.addListener(({ value }) => this.setState(() => ({ animationValue: value })))
   }
+
+  frontInterpolate = () => {
+    return this.state.rotate.interpolate({
+      inputRange: [0, 180],
+      outputRange: ['0deg', '180deg']
+    })
+  };
+
+  backInterpolate = () => {
+    return this.state.rotate.interpolate({
+      inputRange: [0, 180],
+      outputRange: ['180deg', '360deg']
+    })
+  };
 
   nextQuestion = () => {
     this.setState(prevState => ({
@@ -49,18 +73,71 @@ class Quiz extends Component {
     }) );
   };
 
+  flipCard = () => {
+    if ( this.state.animationValue >= 90 ) {
+      Animated.spring(this.state.rotate, {
+        toValue: 0,
+        friction: 8,
+        tension: 10
+      }).start();
+      this.nextQuestion();
+    } else {
+      Animated.spring(this.state.rotate, {
+        toValue: 180,
+        friction: 8,
+        tension: 10
+      }).start();
+    }
+  }
+
   render() {
-    const { questions, gradient } = this.props;
-    console.log(gradient);
+    const { questions } = this.props;
     const { questionCounter } = this.state;
-    const question = questions[questionCounter] ? questions[questionCounter].question : null;
+    let question = null;
+    let answer = null;
+    if (questions[questionCounter]) {
+      question = questions[questionCounter].question
+      answer = questions[questionCounter].answer
+    }
+    const frontAnimatedStyle = {
+      transform: [
+        { rotateY: this.frontInterpolate() }
+      ]
+    };
+    const backAnimatedStyle = {
+      transform: [
+        { rotateY: this.backInterpolate() }
+      ]
+    }
 
     return (
       <View style={{flex: 1, alignItems: 'center'}}>
-        <QuizCard color={this.props.gradient[1]} style={{shadowOffset: {height: 10}, width: .95 * Dimensions.get('window').width}}>
-          {question ? <Title>{question}</Title> : <Title>Done</Title> }
-        </QuizCard>
-        <AnswerButton color={this.props.gradient[1]}><Text style={{textAlign: 'center', color: this.props.gradient[1], fontSize: 20}}>View Answer</Text></AnswerButton>
+        <View>
+          <Animated.View 
+            style={[styles.QuizCard, frontAnimatedStyle, {backgroundColor: this.props.gradient[1]}]}>
+              {question ? <Title>{question}</Title> : <Title>Done</Title> }
+              <Text style={{marginTop: 20}}>
+              {questionCounter + 1 > questions.length ? questions.length : questionCounter + 1} / {questions.length}
+              </Text>
+          </Animated.View>
+          <Animated.View
+            color={this.props.gradient[1]}
+            style={[styles.QuizCard, backAnimatedStyle, {backgroundColor: this.props.gradient[1], position: 'absolute', top: 0, left: 0}]}>
+              {answer ? <Title>{answer}</Title> : <Title>Answer Done</Title>}
+            <Text style={{marginTop: 20}}>
+              {questionCounter + 1 > questions.length ? questions.length : questionCounter + 1} / {questions.length}
+            </Text>
+          </Animated.View>
+        </View>
+        <AnswerButton 
+          color={this.props.gradient[1]}
+          onPress={this.flipCard}>
+            <Text style={{textAlign: 'center', color: this.props.gradient[1], fontSize: 20}}>
+            { questionCounter + 1 === questions.length && this.state.animationValue >= 90
+              ? 'View Results'
+              : this.state.animationValue >= 90 ? 'Next Question' : 'View Answer' }
+            </Text>
+        </AnswerButton>
       </View>
     );
   };
